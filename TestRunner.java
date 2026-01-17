304f0df0dcfd852d63d8db40f66eea46629ca930
Jesse Hernandez <69186875+jayx-create@users.noreply.github.com>
2026-01-16 21:34:23 -0600

    Create TestRunner.java

---
+++ TestRunner.java
@@ -0,0 +1,11 @@
+package com.peerpay.runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com.peerpay.steps",
+        plugin = {"pretty", "html:target/cucumber-reports.html"}
+)
+public class TestRunner extends AbstractTestNGCucumberTests {}
